76ec01037a8cce83bb51127f1a2b83c441986bd9
MKS-01 <manish10mks@gmail.com>
2020-09-19 13:03:20 +0530

    Initial Commit

---
+++ Memorize-Standford/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Memorize-Standford
+//
+//  Created by MKS on 19/09/20.
+//  Copyright © 2020 MKS. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
