0bdb60c219702f683787d857ed4dd21f8e86e030
MKS-01 <manish10mks@gmail.com>
2020-09-23 16:52:03 +0530

    lecture 3..

--- Memorize-Standford/EmojiMemoryGame.swift
+++ Memorize-Standford/EmojiMemoryGame.swift
@@ -10,8 +10,8 @@ import SwiftUI
 
 
 
-class EmojiMemoryGame {
-    private var model: MemoryGame<String> = EmojiMemoryGame.createMemoryGame()
+class EmojiMemoryGame: ObservableObject {
+    @Published private var model: MemoryGame<String> = EmojiMemoryGame.createMemoryGame()
     
     static func createMemoryGame() -> MemoryGame<String> {
         let emojis: Array<String>  = ["👻","🎃","🕷"]
@@ -19,6 +19,8 @@ class EmojiMemoryGame {
             return emojis[pairIndex]
         }
     }
+    
+    
         
     
     // MARK: - Access to the Model
